038d640981302fd51792e2b7a4ffaa5266f6bf37
César Cordeiro <cesar88.20@gmail.com>
2024-08-15 16:17:17 -0300

    Reformat code

--- 2023/day_2/day_2.c
+++ 2023/day_2/day_2.c
@@ -17,116 +17,116 @@ static int possible_game_amount = 0;
 
 int extract_game_id(char* line)
 {
-    int game_number;
+	int game_number;
 
-    sscanf(line, "Game %d:", &game_number);
+	sscanf(line, "Game %d:", &game_number);
 
-    return game_number;
+	return game_number;
 }
 
 // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 void extract_colors(char* line, int* reds, int* greens, int* blues)
 {
-    char* str = line;
+	char* str = line;
 
-    int start_pos;
-    sscanf(str, "Game %*d:%n", &start_pos);
-    str += start_pos;
+	int start_pos;
+	sscanf(str, "Game %*d:%n", &start_pos);
+	str += start_pos;
 
-    char *saved;
-    for (char *p = strtok_r(str, ";", &saved); p != NULL; p = strtok_r(NULL, ";", &saved)) {
-	char *saved2;
-	for (char *s = strtok_r(p, ",", &saved2); s != NULL; s = strtok_r(NULL, ",", &saved2)) {
-	    int amount = 0;
-	    char color[6];
+	char *saved;
+	for (char *p = strtok_r(str, ";", &saved); p != NULL; p = strtok_r(NULL, ";", &saved)) {
+		char *saved2;
+		for (char *s = strtok_r(p, ",", &saved2); s != NULL; s = strtok_r(NULL, ",", &saved2)) {
+			int amount = 0;
+			char color[6];
 
-	    sscanf(s, " %d %s", &amount, color);
+			sscanf(s, " %d %s", &amount, color);
 
-	    if(strcmp(color, "blue") == 0){
-		*blues = amount;
-		blues++;
-	    } else if(strcmp(color, "green") == 0) {
-		*greens = amount;
-		greens++;
-	    } else if(strcmp(color, "red") == 0) {
-		*reds = amount;
-		reds++;
-	    }
+			if(strcmp(color, "blue") == 0){
+				*blues = amount;
+				blues++;
+			} else if(strcmp(color, "green") == 0) {
+				*greens = amount;
+				greens++;
+			} else if(strcmp(color, "red") == 0) {
+				*reds = amount;
+				reds++;
+			}
+		}
 	}
-    }
 
 }
 
 bool check_possible_color(int* color, int max)
 {
-    int* ptr = color;
-    while(*ptr > -1) {
-	if (*ptr > max)
-	    return false;
-	ptr++;
-    }
+	int* ptr = color;
+	while(*ptr > -1) {
+		if (*ptr > max)
+			return false;
+		ptr++;
+	}
 
-    return true;
+	return true;
 }
 
 bool check_possible(int* reds, int* greens, int* blues)
 {
-    return check_possible_color(reds, MAX_RED) && \
-	check_possible_color(greens, MAX_GREEN) && \
-	check_possible_color(blues, MAX_BLUE);
+	return check_possible_color(reds, MAX_RED) && \
+		check_possible_color(greens, MAX_GREEN) && \
+		check_possible_color(blues, MAX_BLUE);
 }
 
 int* create_array_for_color(int size)
 {
-    int colors[size];
-    int* ptr = colors;
+	int colors[size];
+	int* ptr = colors;
 
-    for (int i = 0; i < size; i++) {
-	colors[i] = -1;
-    }
+	for (int i = 0; i < size; i++) {
+		colors[i] = -1;
+	}
 
-    return ptr;
+	return ptr;
 }
 
 void init_array(int* array)
 {
-    for (int i = 0; i < ARRAY_CAP; i++) {
-	array[i] = -1;
-    }
+	for (int i = 0; i < ARRAY_CAP; i++) {
+		array[i] = -1;
+	}
 }
 
 void process_game(char* line)
 {
-    int game_id = extract_game_id(line);
+	int game_id = extract_game_id(line);
 
-    int reds[ARRAY_CAP];
-    init_array(reds);
-    int greens[ARRAY_CAP];
-    init_array(greens);
-    int blues[ARRAY_CAP];
-    init_array(blues);
+	int reds[ARRAY_CAP];
+	init_array(reds);
+	int greens[ARRAY_CAP];
+	init_array(greens);
+	int blues[ARRAY_CAP];
+	init_array(blues);
 
-    extract_colors(line, reds, greens, blues);
+	extract_colors(line, reds, greens, blues);
 
-    bool possible = check_possible(reds, greens, blues);
+	bool possible = check_possible(reds, greens, blues);
 
-    if(possible){
-	possible_game_amount += game_id;
-    }
+	if(possible){
+		possible_game_amount += game_id;
+	}
 }
 
 int main(int argv, char **argc)
 {
-    char *line = NULL;
-    size_t len = 0;
+	char *line = NULL;
+	size_t len = 0;
 
-    while (getline(&line, &len, stdin) != -1) {
-      process_game(line);
-    }
+	while (getline(&line, &len, stdin) != -1) {
+		process_game(line);
+	}
 
-    printf("%d\n", possible_game_amount);
+	printf("%d\n", possible_game_amount);
 
-    free(line);
+	free(line);
 
-    return 0;
+	return 0;
 }
