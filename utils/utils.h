df71b6982815789de090fc4eae6e7968979eb49a
César Cordeiro <cesar88.20@gmail.com>
2024-08-28 18:46:28 -0300

    Dealing with negative numbers

--- utils/utils.h
+++ utils/utils.h
@@ -10,15 +10,26 @@ long convert_integer(char* number_as_string)
 	char *ptr = number_as_string;
 
 	long number = 0;
+	int negative_number = 0;
 	while(*ptr != '\0' && *ptr != '\n') {
 		if (*ptr == ' ') {
 			ptr++;
 			continue;
 		}
+
+		if (*ptr == '-') {
+			negative_number = 1;
+			ptr++;
+			continue;
+		}
+
 		// Using ASCII arithmetics.
 		number = number * 10 + (*ptr - 48);
 		ptr++;
 	}
+
+	if (negative_number == 1)
+		number *= -1;
 
 	return number;
 }
